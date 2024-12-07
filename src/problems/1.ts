3b797853101e5fce507e96fc6d2675ac6b76c41a
noyonalways <noyonrahman2003@gmail.com>
2024-12-07 23:56:16 +0600

    feat: added list of problems

---
+++ src/problems/1.ts
@@ -0,0 +1,23 @@
+/* 
+===== Task: Array Filtering and Mapping =====
+Create an array of objects, each representing a person with properties like name, age, and gender. Write a function to filter out all females and then map the remaining people to an array of names. Print the final result. 
+*/
+
+interface IPerson {
+  name: string;
+  age: number;
+  gender: "male" | "female";
+}
+
+const peoples: IPerson[] = [
+  { name: "Rahim", age: 25, gender: "male" },
+  { name: "Karim", age: 30, gender: "male" },
+  { name: "Fatima", age: 28, gender: "female" },
+  { name: "Ayesha", age: 22, gender: "female" },
+  { name: "Hasan", age: 35, gender: "male" },
+  { name: "Nusrat", age: 27, gender: "female" },
+  { name: "Shakib", age: 40, gender: "female" },
+  { name: "Sumaiya", age: 24, gender: "female" },
+  { name: "Imran", age: 32, gender: "male" },
+  { name: "Rashida", age: 29, gender: "female" },
+];
